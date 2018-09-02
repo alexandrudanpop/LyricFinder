c90c2f2c02919163ad20eaa6808398953a56d752
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2018-09-02 16:25:29 +0300

    fetch+display lyrics + goBack

---
+++ src/components/tracks/Lyrics.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Spinner from "../layout/Spinner";
+
+const api_key = "45c7b2fb9f4bdd2b54765bf91dd3e9a2";
+const cors_bypass = "https://cors-anywhere.herokuapp.com";
+
+class Lyrics extends Component {
+  state = {
+    track: {},
+    lyrics: {}
+  };
+
+  componentDidMount = async () => {
+    const { id } = this.props.match.params;
+    const { data } = await axios.get(
+      `${cors_bypass}/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${id}&apikey=${api_key}`
+    );
+
+    this.setState({
+      ...this.state,
+      lyrics: data.message.body.lyrics
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Lyrics</h1>
+        {this.state.lyrics.lyrics_body ? (
+          <React.Fragment>
+            <p> {this.state.lyrics.lyrics_body}</p>
+            <button
+              className="btn btn-info"
+              onClick={() => this.props.history.push("/")}
+            >
+              Back
+            </button>
+          </React.Fragment>
+        ) : (
+          <Spinner />
+        )}
+      </div>
+    );
+  }
+}
+
+export default Lyrics;
