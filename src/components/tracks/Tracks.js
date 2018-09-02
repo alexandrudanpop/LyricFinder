134ac15aa58bf88b202c4f6c1dcf6690d8a5fb3d
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2018-09-02 18:45:47 +0300

    remove heading from tracks

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -7,14 +7,13 @@ const Tracks = () => {
   return (
     <Consumer>
       {value => {
-        const { track_list, heading } = value;
+        const { track_list } = value;
 
         if (!track_list || track_list.length === 0) {
           return <Spinner />;
         } else {
           return (
             <React.Fragment>
-              <h3 className="text-center mb-4"> {heading} </h3>
               <div className="row">
                 {track_list.map((t, i) => (
                   <Track key={i} track={t.track} />
