211efdb7ec6215d592d3a587a91fa7f27fa942b8
Alexandru-Dan Pop <alexandrudanpop@gmail.com>
2018-09-16 18:55:16 +0300

    footer fixes and change gh pages to master

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,30 +1,33 @@
 import React from "react";
 import "./footer.css";
 
-const Footer = () => {
-  return (
-    <footer className="footer">
-      <div className="container text-center">
-        Copyright{" "}
-        <a
-          className="footer-a"
-          href="http://www.control-f5.com/"
-          target="_blank"
-        >
-          Control-F5.com {new Date().getFullYear()}
-        </a>
-        . Powered by{" "}
-        <a
-          className="footer-a"
-          style={{ textDecoration: "none" }}
-          href="https://developer.musixmatch.com/documentation"
-          target="_blank"
-        >
-          Musixmatch lyrics API
-        </a>
-      </div>
-    </footer>
-  );
-};
+const Footer = () => (
+  <footer className="footer">
+    <div
+      className="container text-center"
+      style={{ backgroundColor: "#f5f5f5" }}
+    >
+      Copyright{" "}
+      <a
+        className="footer-a"
+        href="http://www.control-f5.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Control-F5.com {new Date().getFullYear()}
+      </a>
+      . Powered by{" "}
+      <a
+        className="footer-a"
+        style={{ textDecoration: "none" }}
+        href="https://developer.musixmatch.com/documentation"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Musixmatch lyrics API
+      </a>
+    </div>
+  </footer>
+);
 
 export default Footer;
